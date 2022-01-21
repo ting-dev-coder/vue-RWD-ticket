8fe86befbb28e4a00902e7707c0a92066ac088bc
tp27933 <missjoker59@gmail.com>
2022-01-21 10:35:18 +0800

    [modified] config

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,11 +1,19 @@
 <template>
-  <div class="mask" v-show="dispaly" @click="$emit('update:modelValue', false)"/>
   <div
-    class="sideBar px-3"
-    :class="{active:dispaly}"
-  >
-    <header class="d-flex justify-content-between align-items-center mb-4 py-2 border-bottom">
-      <button type="button" class="btn-close btn-close-white" aria-label="Close" @click="$emit('update:modelValue', false)"></button>
+    class="mask"
+    v-show="dispaly"
+    @click="$emit('update:modelValue', false)"
+  />
+  <div class="sideBar px-3" :class="{ active: dispaly }">
+    <header
+      class="d-flex justify-content-between align-items-center mb-4 py-2 border-bottom"
+    >
+      <button
+        type="button"
+        class="btn-close btn-close-white"
+        aria-label="Close"
+        @click="$emit('update:modelValue', false)"
+      />
       <h2 class="text-gray-300 mb-0">購物車</h2>
     </header>
     <div v-if="list.length === 0">
@@ -14,33 +22,60 @@
         href="#"
         src="/shopping/order"
         class="btn bg-primary mt-2"
-        @click.prevent="$emit('hiddePopover','Order')"
+        @click.prevent="$emit('hiddePopover', 'Order')"
         >訂票去</a>
     </div>
     <ul class="list-wrap" v-else>
-      <li v-for="(item, idx) in list" :key="'item_' + idx" class="list my-4 row">
-        <div class="col-4 avatar" :style="`background-image: url(${item.product.imageUrl})`"></div>
+      <li
+        v-for="(item, idx) in list"
+        :key="'item_' + idx"
+        class="list my-4 row"
+      >
+        <div
+          class="col-4 avatar"
+          :style="`background-image: url(${item.product.imageUrl})`"
+        ></div>
         <div class="px-3 text-start text-white col-6">
           <h4 class="fs-6 mb-3 text-white">{{ item.product.title }}</h4>
           <div class="mb-3">
-            <a href="#" class="text-white px-2 align-middle fs-5" type="button" :class="{ disabled: item.qty <= 1 }"   @click.prevent="changeQty(item, -1)">-</a>
-              <input
-                type="number"
-                v-model="item.qty"
-                min="0"
-                class="text-gray-800 text-center fs-5"
-                aria-describedby="inputGroup-sizing-default"
-              />
-            <a href="#" class="text-white px-2 align-middle fs-5" type="button"  @click.prevent="changeQty(item, +1)">+</a>
+            <a
+              href="#"
+              class="text-white px-2 align-middle fs-5"
+              type="button"
+              :class="{ disabled: item.qty <= 1 }"
+              @click.prevent="changeQty(item, -1)"
+              >-</a>
+            <input
+              type="number"
+              v-model="item.qty"
+              min="0"
+              class="text-gray-800 text-center fs-5"
+              aria-describedby="inputGroup-sizing-default"
+            />
+            <a
+              href="#"
+              class="text-white px-2 align-middle fs-5"
+              type="button"
+              @click.prevent="changeQty(item, +1)"
+              >+</a>
           </div>
           <p class="fs-5">NT$ {{ item.total }}</p>
         </div>
-        <div class="fs-5 col-2 d-flex justify-content-center align-items-center" @click="onDelProductlClick(item)"><font-awesome-icon class="pointer"  icon="trash-alt" /></div>
+        <div
+          class="fs-5 col-2 d-flex justify-content-center align-items-center"
+          @click="onDelProductlClick(item)"
+        >
+          <font-awesome-icon class="pointer" icon="trash-alt" />
+        </div>
       </li>
     </ul>
     <div class="border-top">
       <p>合計: {{ total }}</p>
-      <button class="w-100 border-0 btn bg-primary" @click="$emit('hiddePopover','Cart')" type="button">
+      <button
+        class="w-100 border-0 btn bg-primary"
+        @click="$emit('hiddePopover', 'Cart')"
+        type="button"
+      >
         檢視購物車
       </button>
     </div>
@@ -71,12 +106,16 @@ export default {
     const list = reactive([])
     const total = ref(0)
     const dispaly = ref(false)
-    watch(props.carts, (newVal) => {
-      total.value = newVal.total
-      list.length = 0
-      list.push(...newVal.list)
-    }, { immediate: true })
-    watch(modelValue, (newVal) => {
+    watch(
+      props.carts,
+      newVal => {
+        total.value = newVal.total
+        list.length = 0
+        list.push(...newVal.list)
+      },
+      { immediate: true }
+    )
+    watch(modelValue, newVal => {
       dispaly.value = newVal
     })
     const changeQty = (i, operation) => {
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,15 @@
 const path = require('path')
 const resolve = (dir) => path.join(__dirname, dir)
 module.exports = {
+  devServer: {
+    port: 8080, // 设置端口号
+    host: '192.168.5.23', // ip
+    disableHostCheck: true, // 是否关闭用于 DNS 重绑定的 HTTP 请求的 HOST 检查
+    hotOnly: false, // 热更新
+    https: false, // https:{type:Boolean}配置前缀
+    open: false, // 配置自动启动浏览器
+    proxy: null // 设置代理
+  },
   publicPath: process.env.NODE_ENV === 'production' ? '/ticket/' : '/',
   chainWebpack: config => {
     config.resolve.alias
